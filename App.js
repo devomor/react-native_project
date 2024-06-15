e5986da99a29570f7c876993f5faa3d64caea6a7
softdevomar <omorfarukqqq@gmail.com>
2024-06-14 22:02:15 -0700

    react native component create

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import React, {Component} from 'react';
-import {View, Text } from 'react-native';
+import {View, Text, StyleSheet, ActivityIndicator } from 'react-native';
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Text style={{color:'red',fontSize:30 }}>hellow aima progormar my name omar</Text>
+      //card style add
+      <View style={styles.card}>
+        <Text style={{color:'blue',fontSize:30 }}>hellow aima progormar my name omar</Text>
         <Text>omor faruk hellow proggramer</Text>
-        <Text>omor faruk hellow proggramer</Text>
+          <ActivityIndicator size="large" color="#00ff00" />
+          
       </View>
+      
     );
   }
 }
+  const styles = StyleSheet.create({
+    card: {
+    width: 500,
+    borderRadius: 16,
+    marginVertical: 6,
+    backgroundColor:'red',
+    alignItems:"center",
+
+  },
+    horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
+  },
+  });
