a0c1f0f265d21de22e2c66d8a3596aa02759caf4
softdevomar <omorfarukqqq@gmail.com>
2024-06-14 20:24:11 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {View, Text } from 'react-native';
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={{color:'red',fontSize:30 }}>hellow aima progormar my name omar</Text>
+        <Text>omor faruk hellow proggramer</Text>
+        <Text>omor faruk hellow proggramer</Text>
+      </View>
+    );
+  }
+}
